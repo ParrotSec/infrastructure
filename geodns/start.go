0cdd9bca63eaa26dd3b6da8c7f780764cfede349
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2019-07-25 03:43:42 +0200

    update geodns start script

--- geodns/start.go
+++ geodns/start.go
@@ -6,18 +6,36 @@ import (
 )
 
 func runPdns(c chan int) {
-	exec.Command("/usr/sbin/pdns_server", "--guardian=no", "--daemon=no")
-	c <- 1
+	i, err := exec.Command("/usr/sbin/pdns_server", "--guardian=no", "--daemon=no", "--write-pid=no", "--disable-syslog").Output()
+	if err != nil {
+		fmt.Printf("Error: %s\n\n%s", err, i)
+		c <- 2
+	} else {
+		fmt.Printf("\n%s\n", i)
+		c <- 1
+	}
 }
 
 func runRecursor(c chan int) {
-	exec.Command("/usr/sbin/pdns_recursor", "--guardian=no", "--daemon=no")
-	c <- 1
+	i, err := exec.Command("/usr/sbin/pdns_recursor", "--guardian=no", "--daemon=no", "--write-pid=no", "--disable-syslog").Output()
+	if err != nil {
+		fmt.Printf("Error: %s\n\n%s", err, i)
+		c <- 2
+	} else {
+		fmt.Printf("\n%s\n", i)
+		c <- 1
+	}
 }
 
 func runDnsdist(c chan int) {
-	exec.Command("/usr/bin/dnsdist", "--supervised", "-u", " _dnsdist", "-g", "_dnsdist")
-	c <- 1
+	i, err := exec.Command("/usr/bin/dnsdist", "--supervised").Output()
+	if err != nil {
+		fmt.Printf("Error: %s\n\n%s", err, i)
+		c <- 2
+	} else {
+		fmt.Printf("\n%s\n", i)
+		c <- 1
+	}
 }
 
 func main() {
@@ -32,14 +50,11 @@ func main() {
 	for true {
 		select {
 		case p := <-pdns:
-			fmt.Printf("powerdns authoritative server exited, restarting it: %v\n", p)
-			go runPdns(pdns)
+			fmt.Printf("powerdns authoritative server exited: %v\n", p)
 		case r := <-recursor:
-			fmt.Printf("powerdns recursor exited, restarting it: %v\n", r)
-			go runRecursor(recursor)
+			fmt.Printf("powerdns recursor exited: %v\n", r)
 		case d := <-dnsdist:
-			fmt.Printf("dnsdist dns load balancer exited, restarting it: %v\n", d)
-			go runDnsdist(dnsdist)
+			fmt.Printf("dnsdist dns load balancer exited: %v\n", d)
 		}
 	}
 }
