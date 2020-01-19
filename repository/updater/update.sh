11f965adbcbf1a94fe52b2a0b758fdfa6441c26d
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-01-19 04:41:52 +0100

    add first repository draft

---
+++ repository/updater/update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SOURCE_PARROT=master.rsync.parrot.sh/parrot
+TARGET_PARROT=/var/www/parrot
+SOURCE_ALL=master.rsync.parrot.sh/internal
+TARGET_ALL=/var/www
+
+while true; do
+	#flock -xn /tmp/parrot-rsync.lock -c "rsync -PahvHtSx --delay-updates --delete-after master.rsync.parrot.sh/parrot /var/www/html/parrot"
+	touch $TARGET_PARROT/SYNC_IN_PROGRESS.txt || true
+	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.pool.txt -c "rsync -qaHtSx --exclude=dists --exclude=iso $SOURCE_PARROT $TARGET_PARROT/"
+	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.dists.txt -c "rsync -qaHtSx --exclude=iso --delay-updates --delete-after $SOURCE_PARROT $TARGET_PARROT/"
+	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.iso.txt -c "rsync -qaHtSx --exclude=dists --exclude=pool --delay-updates $SOURCE_PARROT $TARGET_PARROT/"
+	rm $TARGET_PARROT/SYNC_IN_PROGRESS.txt || true
+	sleep 600
+done &
+
+while true; do
+	flock -xn $TARGET_ALL/SYNC_IN_PROGRESS.txt -c "rsync -qaHtSx --exclude=dists --exclude=iso $SOURCE_PARROT $TARGET_PARROT/"
+	sleep 3600
+done
