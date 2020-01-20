4260ffc82df0dd27027eeb351f56cfcc124106c1
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-01-20 16:04:44 +0100

    praise the sun

--- repository/updater/update.sh
+++ repository/updater/update.sh
@@ -7,15 +7,16 @@ TARGET_ALL=/var/www/html
 
 while true; do
 	#flock -xn /tmp/parrot-rsync.lock -c "rsync -PahvHtSx --delay-updates --delete-after master.rsync.parrot.sh/parrot /var/www/html/parrot"
-	touch $TARGET_PARROT/SYNC_IN_PROGRESS.txt || true
-	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.pool.txt -c "rsync -qaHtSx --exclude=dists --exclude=iso $SOURCE_PARROT $TARGET_PARROT/"
-	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.dists.txt -c "rsync -qaHtSx --exclude=iso --delay-updates --delete-after $SOURCE_PARROT $TARGET_PARROT/"
-	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.iso.txt -c "rsync -qaHtSx --exclude=dists --exclude=pool --delay-updates $SOURCE_PARROT $TARGET_PARROT/"
-	rm $TARGET_PARROT/SYNC_IN_PROGRESS.txt || true
+	touch $TARGET_PARROT/SYNC_IN_PROGRESS.lock || true
+	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.pool.lock -c "rsync -qaHtSx --exclude=dists --exclude=iso $SOURCE_PARROT $TARGET_PARROT/"
+	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.dists.lock -c "rsync -qaHtSx --exclude=iso --delay-updates --delete-after $SOURCE_PARROT $TARGET_PARROT/"
+	flock -xn $TARGET_PARROT/SYNC_IN_PROGRESS.iso.lock -c "rsync -qaHtSx --exclude=dists --exclude=pool --delay-updates $SOURCE_PARROT $TARGET_PARROT/"
+	rm $TARGET_PARROT/SYNC_IN_PROGRESS.* || true
 	sleep 600
 done &
 
 while true; do
-	flock -xn $TARGET_ALL/SYNC_IN_PROGRESS.txt -c "rsync -qaHtSx --exclude=parrot $SOURCE_PARROT $TARGET_PARROT/"
+	flock -xn $TARGET_ALL/SYNC_IN_PROGRESS.lock -c "rsync -qaHtSx --exclude=parrot $SOURCE_ALL $TARGET_ALL/"
+	rm $TARGET_ALL/SYNC_IN_PROGRESS.* || true
 	sleep 3600
 done
