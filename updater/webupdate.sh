33c36c18b34c11f1707b0b6b99395c91b0514d3c
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-04-20 18:32:20 +0200

    keep it simple

--- updater/webupdate.sh
+++ updater/webupdate.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-export SITE_SOURCE="/ipns/www.parrotsec.org"
-export SITE_TARGET="/var/www/html"
-export DOCS_SOURCE="/ipns/docs.parrotsec.org"
-export DOCS_TARGET="/var/www/docs"
-export STATIC_SOURCE="/ipns/static.parrotsec.org"
-export STATIC_TARGET="/var/www/static"
-
-
 while true; do
-    ipfs --api="/ip4/172.18.44.2/tcp/5001" get $(SITE_SOURCE) -o $(SITE_TARGET)
-    ipfs --api="/ip4/172.18.44.2/tcp/5001" get $(DOCS_SOURCE) -o $(DOCS_TARGET)
-    ipfs --api="/ip4/172.18.44.2/tcp/5001" get $(STATIC_SOURCE) -o $(STATIC_TARGET)
+    ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/www.parrotsec.org -o /var/www/html
+    ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/docs.parrotsec.org -o /var/www/docs
+    #ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/static.parrotsec.org -o /var/www/static
     sleep 300
 done
