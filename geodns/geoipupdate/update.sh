59793f40d029de41532cf868f792a218796a2fdc
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-04-20 16:27:01 +0200

    fix ipdb updater

--- geodns/geoipupdate/update.sh
+++ geodns/geoipupdate/update.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 while true; do
-	geoipupdate --config-file /etc/GeoIP.conf  --database-directory /var/lib/GeoIP/
-	wget -qO - https://dl.miyuru.lk/geoip/dbip/city/dbip4.dat.gz | gzip -d > /var/lib/GeoIP/dbipcity4.dat.temp || true 
-	wget -qO - https://dl.miyuru.lk/geoip/dbip/city/dbip6.dat.gz | gzip -d > /var/lib/GeoIP/dbipcity6.dat.temp || true
+        geoipupdate --config-file /etc/GeoIP.conf  --database-directory /var/lib/GeoIP/
+        wget -qO - https://dl.miyuru.lk/geoip/dbip/city/dbip4.dat.gz | gzip -d > /var/lib/GeoIP/dbipcity4.dat.temp || true 
+        wget -qO - https://dl.miyuru.lk/geoip/dbip/city/dbip6.dat.gz | gzip -d > /var/lib/GeoIP/dbipcity6.dat.temp || true
 
-	v4cursize=$(wc -c < dbipcity4.dat)
-	v6cursize=$(wc -c < dbipcity6.dat)
-	v4newsize=$(wc -c < dbipcity4.dat)
-	v6newsize=$(wc -c < dbipcity6.dat)
-	if [ $v4newsize -ge 1567236 ]; then
-		mv /var/lib/GeoIP/dbipcity4.dat.temp /var/lib/GeoIP/dbipcity4.dat || true
-	fi
-	if [ $v6newsize -ge 1567236 ]; then
-		mv /var/lib/GeoIP/dbipcity6.dat.temp /var/lib/GeoIP/dbipcity6.dat || true
-	fi
-	rm /var/lib/GeoIP/*.temp || true
-	sleep 86400
+        v4cursize=$(wc -c < /var/lib/GeoIP/dbipcity4.dat)
+        v6cursize=$(wc -c < /var/lib/GeoIP/dbipcity6.dat)
+        v4newsize=$(wc -c < /var/lib/GeoIP/dbipcity4.dat.temp)
+        v6newsize=$(wc -c < /var/lib/GeoIP/dbipcity6.dat.temp)
+        if [ $v4newsize -ge 1567236 ]; then
+                mv /var/lib/GeoIP/dbipcity4.dat.temp /var/lib/GeoIP/dbipcity4.dat || true
+        fi
+        if [ $v6newsize -ge 1567236 ]; then
+                mv /var/lib/GeoIP/dbipcity6.dat.temp /var/lib/GeoIP/dbipcity6.dat || true
+        fi
+        rm /var/lib/GeoIP/*.temp || true
+        sleep 86400
 done
