0b0b285f09e89d2c9507f02b23a61a57309279b9
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-04-20 16:58:59 +0200

    add website updater

---
+++ updater/webupdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SITE_SOURCE="/ipns/www.parrotsec.org"
+SITE_TARGET="/var/www/html"
+DOCS_SOURCE="/ipns/docs.parrotsec.org"
+DOCS_TARGET="/var/www/docs"
+STATIC_SOURCE="/ipns/static.parrotsec.org"
+STATIC_TARGET="/var/www/static"
+
+
+while true; do
+	ipfs get $SITE_SOURCE -o $SITE_TARGET
+    ipfs get $DOCS_SOURCE -o $DOCS_TARGET
+    ipfs get $STATIC_SOURCE -o $STATIC_TARGET
+	sleep 300
+done
