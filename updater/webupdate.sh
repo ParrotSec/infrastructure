350a61935f45ecba4e543a48702404184cf128aa
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-04-20 18:24:51 +0200

    minor fixes

--- updater/webupdate.sh
+++ updater/webupdate.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-SITE_SOURCE="/ipns/www.parrotsec.org"
-SITE_TARGET="/var/www/html"
-DOCS_SOURCE="/ipns/docs.parrotsec.org"
-DOCS_TARGET="/var/www/docs"
-STATIC_SOURCE="/ipns/static.parrotsec.org"
-STATIC_TARGET="/var/www/static"
+export SITE_SOURCE="/ipns/www.parrotsec.org"
+export SITE_TARGET="/var/www/html"
+export DOCS_SOURCE="/ipns/docs.parrotsec.org"
+export DOCS_TARGET="/var/www/docs"
+export STATIC_SOURCE="/ipns/static.parrotsec.org"
+export STATIC_TARGET="/var/www/static"
 
 
 while true; do
-	ipfs --api=/ip4/172.18.44.2/tcp/5001 get $SITE_SOURCE -o $SITE_TARGET
-    ipfs --api=/ip4/172.18.44.2/tcp/5001 get $DOCS_SOURCE -o $DOCS_TARGET
-    ipfs --api=/ip4/172.18.44.2/tcp/5001 get $STATIC_SOURCE -o $STATIC_TARGET
-	sleep 300
+    ipfs --api="/ip4/172.18.44.2/tcp/5001" get $(SITE_SOURCE) -o $(SITE_TARGET)
+    ipfs --api="/ip4/172.18.44.2/tcp/5001" get $(DOCS_SOURCE) -o $(DOCS_TARGET)
+    ipfs --api="/ip4/172.18.44.2/tcp/5001" get $(STATIC_SOURCE) -o $(STATIC_TARGET)
+    sleep 300
 done
