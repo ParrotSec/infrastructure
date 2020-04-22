fcdee5109be5d4fcec48c6e5f5a32ef4252f7e44
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-04-23 01:53:14 +0200

    improve website update method

--- updater/webupdate.sh
+++ updater/webupdate.sh
@@ -4,5 +4,6 @@ while true; do
     ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/www.parrotsec.org -o /var/www/html
     ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/docs.parrotsec.org -o /var/www/docs
     #ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/static.parrotsec.org -o /var/www/static
+    rsync -qa --delete-after /var/www/docs/ /var/www/html/docs/
     sleep 60
 done
