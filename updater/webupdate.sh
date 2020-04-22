ab795a1d060d7a2bffa83f463b96f0c8c288fb84
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-04-23 01:46:48 +0200

    decrease update rate of website

--- updater/webupdate.sh
+++ updater/webupdate.sh
@@ -4,5 +4,5 @@ while true; do
     ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/www.parrotsec.org -o /var/www/html
     ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/docs.parrotsec.org -o /var/www/docs
     #ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/static.parrotsec.org -o /var/www/static
-    sleep 300
+    sleep 60
 done
