f84abb2665ff657db08faf315fa18b025ad4e290
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2020-12-07 13:26:44 +0100

    disable ipfs and use rsync to update website

--- updater/webupdate.sh
+++ updater/webupdate.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 while true; do
-    ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/www.parrotsec.org -o /var/www/html
-    ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/docs.parrotsec.org -o /var/www/docs
-    #ipfs --api="/ip4/172.18.44.2/tcp/5001" get /ipns/static.parrotsec.org -o /var/www/static
-    rsync -qa --delete-after /var/www/docs/ /var/www/html/docs/
+    (
+        rsync -qa --delete-after rsync.parrot.sh:website /var/www/html || \
+        rsync -qa --delete-after repository-2.internal.parrot.sh:website /var/www/html || \
+        rsync -qa --delete-after repository-3.internal.parrot.sh:website /var/www/html
+    )
+
+    (
+        rsync -qa --delete-after rsync.parrot.sh:docs /var/www/docs || \
+        rsync -qa --delete-after repository-2.internal.parrot.sh:docs /var/www/docs || \
+        rsync -qa --delete-after repository-3.internal.parrot.sh:docs /var/www/docs
+    ) && rsync -qa --delete-after /var/www/docs/ /var/www/html/docs/
+
     sleep 600
 done
